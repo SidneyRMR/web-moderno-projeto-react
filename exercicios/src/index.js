1c714039d54c3cbeb4db5ba06aa8fadd62a3b200
Sidney Rafael Mantovani Ribeiro <sidneyrafael191@gmail.com>
2022-08-15 16:53:33 -0300

    Changes to be committed: 	new file:   exercicios/src/components/Filho.jsx 	new file:   exercicios/src/components/Pai.jsx 	modified:   exercicios/src/index.js

--- exercicios/src/index.js
+++ exercicios/src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom'
 
 // import Primeiro from './components/Primeiro'
 // import BomDia from './components/BomDia'
-import {BoaTarde,  BoaNoite} from "./components/Multiplos"
-import Saudacao from './components/Saudacao'
+// import {BoaTarde,  BoaNoite} from "./components/Multiplos"
+import Pai from './components/Pai'
+import Filho from './components/Filho'
 
 ReactDOM.render(
     <div>
-        <BoaTarde nome="Guilherme" idade={10}/>
-        <Saudacao tipo="Bom Dia" nome="João"/>
+        <Pai nome="Paulo" sobrenome='Silva'>
+            {/* {/* <Filho nome="Pedro" sobrenome='Silva'/> */}
+            <Filho nome="Paulo" sobrenome='Silva'/>
+            <Filho nome="Carla" sobrenome='Silva'/> 
+            
+            
+        </Pai>
+
     </div>, document.getElementById('root'))
     
