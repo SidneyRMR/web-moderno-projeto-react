90d7ad20dd9632fc104659e1fa3c72299cf8ad93
Sidney Rafael Mantovani Ribeiro <sidneyrafael191@gmail.com>
2022-09-20 01:47:28 -0300

    Conclusão capitulo de router

--- exercicios/src/index.js
+++ exercicios/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-// import Primeiro from './components/Primeiro'
+import Primeiro from './components/Primeiro'
 // import BomDia from './components/BomDia'
 // import {BoaTarde,  BoaNoite} from "./components/Multiplos"
 import Pai from './components/Pai'
@@ -9,13 +9,7 @@ import Filho from './components/Filho'
 
 ReactDOM.render(
     <div>
-        <Pai nome="Paulo" sobrenome='Silva'>
-            {/* {/* <Filho nome="Pedro" sobrenome='Silva'/> */}
-            <Filho nome="Paulo" sobrenome='Silva'/>
-            <Filho nome="Carla" sobrenome='Silva'/> 
-            
-            
-        </Pai>
-
+        <Primeiro/>
+   
     </div>, document.getElementById('root'))
     
